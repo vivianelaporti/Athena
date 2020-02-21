5fa9ef34c733fe17a8556e482d65900a9d4ecdfb
VIviane Laporti <vivianelaporti@users.noreply.github.com>
2020-02-21 14:40:57 -0300

    Initial commit

---
+++ Application/routes.py
@@ -0,0 +1,16 @@
+from Application import app
+
+from datetime import datetime
+from flask import render_template
+
+@app.route( '/' )
+@app.route( '/index' )
+def index( ):
+    """Renders the home page."""
+    return render_template(
+        'index.html' ,
+        title = 'Athena' ,
+        # year = datetime.now( ).year
+        index = True
+    )
+
