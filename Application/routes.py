47fb1c176ca700771e16160cef95311129aeae43
eduardo <eduardo@kognitus.com.br>
2020-02-21 23:49:46 -0300

    Ajuste

--- Application/routes.py
+++ Application/routes.py
@@ -3,9 +3,20 @@ from Application import app
 from datetime import datetime
 from flask import render_template
 
+# @app.route( '/' )
+# @app.route( '/index' )
+# def index( ):
+#     """Renders the home page."""
+#     return render_template(
+#         'index.html' ,
+#         title = 'Athena' ,
+#         # year = datetime.now( ).year
+#         index = True
+#     )
+
 @app.route( '/' )
 @app.route( '/index' )
-def index( ):
+def lstHistorias( ):
     """Renders the home page."""
     return render_template(
         'index.html' ,
@@ -13,4 +24,3 @@ def index( ):
         # year = datetime.now( ).year
         index = True
     )
-
