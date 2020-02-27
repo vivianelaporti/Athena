7a2263829caf836c3baa36ed40d7b7aea7590805
VIviane Laporti <41812486+vivianelaporti@users.noreply.github.com>
2020-02-27 20:49:22 -0300

    Add files via upload

--- Application/routes.py
+++ Application/routes.py
@@ -16,11 +16,23 @@ from flask import render_template
 
 @app.route( '/' )
 @app.route( '/index' )
-def lstHistorias( ):
+def index( ):
     """Renders the home page."""
     return render_template(
         'index.html' ,
         title = 'Athena' ,
-        # year = datetime.now( ).year
         index = True
     )
+
+@app.route( '/' )
+@app.route( '/lstHistorias' )
+def lstHistorias( ):
+    return render_template(
+        'lstHistorias.html' ,
+        title = 'Athena',
+        lstHistorias = True
+    )
+
+
+
+
